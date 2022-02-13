8cb4a42bc684cf3a95daac9f2c0974ddaf4b7e90
Sourin Das <99353367+SourinDas2000@users.noreply.github.com>
2022-02-13 09:31:52 +0530

    Update Mean Deviation And Standard Deviation.py

--- Mean Deviation And Standard Deviation.py
+++ Mean Deviation And Standard Deviation.py
@@ -1,7 +1,5 @@
 
-# Given below is the frequency distribution of unit test marks of a class of 65 students. The total marks of the test is 20 but the highest marks obtained is 19 while the lowest is 4. Every other student performed inside this range. This program calculates the Mean Deviation and Standard Deviation of the following data.
-
-
+# Given below is the frequency distribution of unit test m
 '''
 
  Frequency distribution of test marks of 60 student :
